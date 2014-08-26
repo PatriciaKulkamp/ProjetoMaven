ad460a4a30fa1459a66a85e09d5e80c7908740b3
Patricia <Patricia@10.94.216.100>
2014-08-26 20:44:32 -0300

    Testando Git

--- GitHub/index.php
+++ GitHub/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-             echo 'Testandoo Git';
+             echo 'Testando Git';
         ?>
     </body>
 </html>
